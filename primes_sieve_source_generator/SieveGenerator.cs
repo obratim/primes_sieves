7b71e0ede8f8731fdf34fbb287f5f53b36afec4e
obratim <none@none>
2021-11-17 00:51:15 +0300

    using BitArray

--- primes_sieve_source_generator/SieveGenerator.cs
+++ primes_sieve_source_generator/SieveGenerator.cs
@@ -20,6 +20,7 @@ namespace primes_sieve_source_generator
             var frame = new Frame(FrameLevel);
             string source = $@"
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -35,7 +36,9 @@ namespace {mainMethod.ContainingNamespace.ToDisplayString()}
             const ulong N = {SieveSize.ToString("### ### ### ### ### ###").Trim().Replace(' ', '_')};
             const ulong MaxNumber = Frame * N;
             var lim = (ulong)(Math.Sqrt(MaxNumber) / Frame) + 1;
-            var sieve = new ulong[N];
+            var sieve = new BitArray[N];
+            for (var i = 0; i < sieve.Length; ++i)
+                sieve[i] = new BitArray({frame.CandidatesPerFrame.Length});
 
             void MarkSieve(ulong prime)
             {{
@@ -43,7 +46,7 @@ namespace {mainMethod.ContainingNamespace.ToDisplayString()}
                 {{
                     switch (n % Frame)
                     {{
-                        {PrintCode(24, (n, i) => $"case {n:0000}: sieve[n / Frame] |= 0x{1UL << i:x4}; break;", frame)}
+                        {PrintCode(24, (n, i) => $"case {n:0000}: sieve[n / Frame].Set({i:0000}, true); break;", frame)}
                     }}
                 }}
             }}
@@ -51,13 +54,13 @@ namespace {mainMethod.ContainingNamespace.ToDisplayString()}
             var j = Frame;
             for (var i = 1U; i <= lim; ++i, j += Frame)
             {{
-                {PrintCode(16, (n, i) => $"if ((sieve[i] & 0x{1UL << i:x4}) == 0) {{ var prime = j + {n:0000}; yield return prime; MarkSieve(prime); }}", frame)}
+                {PrintCode(16, (n, i) => $"if (!sieve[i].Get({i:0000})) {{ var prime = j + {n:0000}; yield return prime; MarkSieve(prime); }}", frame)}
             }}
 
             j = (lim + 1) * Frame;
             for (var i = lim + 1; i < N; ++i, j += Frame)
             {{
-                {PrintCode(16, (n, i) => $"if ((sieve[i] & 0x{1UL << i:x4}) == 0) {{ yield return j + {n:0000}; }}", frame)}
+                {PrintCode(16, (n, i) => $"if (!sieve[i].Get({i:0000})) {{ yield return j + {n:0000}; }}", frame)}
             }}
             
             yield break;
