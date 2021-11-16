998b945a9746d2054ef2525b0061e78de00ea669
obratim <none@none>
2021-11-16 22:06:27 +0300

    generated code works (for FrameLevel = 3)

--- primes_sieve_source_generator/SieveGenerator.cs
+++ primes_sieve_source_generator/SieveGenerator.cs
@@ -32,7 +32,7 @@ namespace {mainMethod.ContainingNamespace.ToDisplayString()}
             {string.Join(" ", frame.PrimesInFrame0.Select(n => $"yield return {n};"))}
 
             const ulong Frame = {frame.FrameVolume};
-            const ulong N = {SieveSize};
+            const ulong N = {SieveSize.ToString("### ### ### ### ### ###").Trim().Replace(' ', '_')};
             const ulong MaxNumber = Frame * N;
             var lim = (ulong)(Math.Sqrt(MaxNumber) / Frame) + 1;
             var sieve = new byte[N];
@@ -43,14 +43,15 @@ namespace {mainMethod.ContainingNamespace.ToDisplayString()}
                 {{
                     switch (n % Frame)
                     {{
-                        case 01: sieve[n / Frame] |= 0x1; break;
+                        {string.Join(" ", Enumerable.Repeat(1UL, 1).Concat(frame.PrimesInFrame0.Skip(frame.PrimesCount)).Select((n, i) => $"case {n}: sieve[n / Frame] |= {1UL << i}; break;"))}
+                        /*case 01: sieve[n / Frame] |= 0x1; break;
                         case 07: sieve[n / Frame] |= 0x2; break;
                         case 11: sieve[n / Frame] |= 0x4; break;
                         case 13: sieve[n / Frame] |= 0x8; break;
                         case 17: sieve[n / Frame] |= 0x10; break;
                         case 19: sieve[n / Frame] |= 0x20; break;
                         case 23: sieve[n / Frame] |= 0x40; break;
-                        case 29: sieve[n / Frame] |= 0x80; break;
+                        case 29: sieve[n / Frame] |= 0x80; break;*/
                     }}
                 }}
             }}
@@ -58,6 +59,8 @@ namespace {mainMethod.ContainingNamespace.ToDisplayString()}
             var j = Frame;
             for (var i = 1U; i <= lim; ++i, j += Frame)
             {{
+                {string.Join(" ", Enumerable.Repeat(1UL, 1).Concat(frame.PrimesInFrame0.Skip(frame.PrimesCount)).Select((n, i) => $"if ((sieve[i] & {1UL << i}) == 0) {{ var prime = j + {n}; yield return prime; MarkSieve(prime); }}"))}
+                /*
                 if ((sieve[i] & 0x01) == 0) {{ var prime = j + 01; yield return prime; MarkSieve(prime); }}
                 if ((sieve[i] & 0x02) == 0) {{ var prime = j + 07; yield return prime; MarkSieve(prime); }}
                 if ((sieve[i] & 0x04) == 0) {{ var prime = j + 11; yield return prime; MarkSieve(prime); }}
@@ -66,11 +69,14 @@ namespace {mainMethod.ContainingNamespace.ToDisplayString()}
                 if ((sieve[i] & 0x20) == 0) {{ var prime = j + 19; yield return prime; MarkSieve(prime); }}
                 if ((sieve[i] & 0x40) == 0) {{ var prime = j + 23; yield return prime; MarkSieve(prime); }}
                 if ((sieve[i] & 0x80) == 0) {{ var prime = j + 29; yield return prime; MarkSieve(prime); }}
+                */
             }}
 
             j = (lim + 1) * Frame;
             for (var i = lim + 1; i < N; ++i, j += Frame)
             {{
+                {string.Join(" ", Enumerable.Repeat(1UL, 1).Concat(frame.PrimesInFrame0.Skip(frame.PrimesCount)).Select((n, i) => $"if ((sieve[i] & {1UL << i}) == 0) {{ var prime = j + {n}; yield return prime; MarkSieve(prime); }}"))}
+                /*
                 if ((sieve[i] & 0x01) == 0) {{ yield return j + 01; }}
                 if ((sieve[i] & 0x02) == 0) {{ yield return j + 07; }}
                 if ((sieve[i] & 0x04) == 0) {{ yield return j + 11; }}
@@ -79,6 +85,7 @@ namespace {mainMethod.ContainingNamespace.ToDisplayString()}
                 if ((sieve[i] & 0x20) == 0) {{ yield return j + 19; }}
                 if ((sieve[i] & 0x40) == 0) {{ yield return j + 23; }}
                 if ((sieve[i] & 0x80) == 0) {{ yield return j + 29; }}
+                */
             }}
             
             yield break;
